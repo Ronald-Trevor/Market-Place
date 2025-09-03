eb9a6baf619e6f5eaf61344395f77259bab7663d
Ronald-Trevor <101838410+Ronald-Trevor@users.noreply.github.com>
2025-09-03 07:14:21 +0300

    Adding the details of the items

--- item/views.py
+++ item/views.py
@@ -4,7 +4,9 @@ from .models import Item
 
 def detail(req,primary_key):
     item = get_object_or_404(Item,pk=primary_key)
+    related_items = Item.objects.filter(category=item.category,is_sold=False).exclude(pk=primary_key)[0:3]
 
     return render(req,'item/detail.html',({
-        'item':item
+        'item':item,
+        'related_items':related_items
     }))
